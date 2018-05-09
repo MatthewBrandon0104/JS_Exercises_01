62697db9ddbb83355a579803f9c3edde0ec490d8
Matthew Brandon <MatthewBrandon0104@gmail.com>
2018-05-08 22:28:34 -0700

    Add files via upload

---
+++ leapYears.js
@@ -0,0 +1,13 @@
+var leapYears = function(year) {
+    if (year % 100 === 0 && year % 400 === 0 && year % 4 === 0) {
+        return true;
+    } else if (year % 100 === 0 && year % 4 === 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = leapYears
